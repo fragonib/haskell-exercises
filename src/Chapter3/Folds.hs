7c5be64945797d7b7ad20e58c0ff35625e54266c
Francisco González Ibáñez <fragonib@gmail.com>
2020-09-07 12:24:00 +0200

    Add minimum by

--- src/Chapter3/Folds.hs
+++ src/Chapter3/Folds.hs
@@ -3,6 +3,8 @@ module Chapter3.Folds where
 import qualified Chapter2.Client as C
 
 
+-- Fold 
+
 data InfNumber a =
     MinusInfinity |
     Number a |
@@ -10,12 +12,11 @@ data InfNumber a =
       deriving Show
 
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr aggregator initial [] = initial
+myFoldr _ initial [] = initial
 myFoldr aggregator initial (x:xs) = aggregator x acum
   where acum = myFoldr aggregator initial xs
 
-
--- Product matching functions
+-- Pattern Matching functions
 
 productPM :: Num a => [a] -> a
 productPM [] = 1
@@ -23,17 +24,35 @@ productPM (x:xs) = x * productPM xs
 
 minimumClientPM :: [C.Client] -> C.Client
 minimumClientPM [client] = client
-minimumClientPM (client:clients) =
-  if length (clientName client) < length (clientName restMinimumClient)
-  then client
-  else restMinimumClient
-    where restMinimumClient = minimumClientPM clients
+minimumClientPM (client:clients) = minClient client (minimumClientPM clients)
+
+allPM :: [Bool] -> Bool
+allPM [condition] = condition
+allPM (condition:conditions) = condition && allPM conditions
+
+-- Folding functions
+
+productFold :: Num a => [a] -> a
+productFold = foldr (*) 1
+
+minimumClientFold :: [C.Client] -> C.Client
+minimumClientFold = foldr1 minClient
+
+allFold :: [Bool] -> Bool
+allFold = foldr (&&) True
+
+minimumBy :: (Ord a) => (a -> a) -> [a] -> a
+minimumBy by = foldr1 (\x y -> if by x < by y then x else y)
+
+
+-- Client comparison
+
+minClient :: C.Client -> C.Client -> C.Client
+minClient firstClient secondClient =
+  if length (clientName firstClient) < length (clientName secondClient)
+  then firstClient else secondClient
 
 clientName :: C.Client -> [Char]
 clientName (C.Individual (C.Person name _ _) _) = name
 clientName (C.Company name _ _ _) = name
 clientName (C.GovOrg name) = name
-
-allPM :: [Bool] -> Bool
-allPM [condition] = condition
-allPM (condition:conditions) = condition && allPM conditions
--- test/Chapter3/FoldsSpec.hs
+++ test/Chapter3/FoldsSpec.hs
@@ -10,6 +10,8 @@ import qualified Chapter3.Folds as SUT
 spec :: Spec
 spec = do
 
+  describe "With pattern matching" $ do
+
     it "product of a integer list" $
       let actual = SUT.productPM [1, 2, 3, 4, 5]
       in actual `shouldBe` 120
@@ -27,3 +29,26 @@ spec = do
       SUT.allPM [False] `shouldBe` False
       SUT.allPM [True, False, True] `shouldBe` False
       SUT.allPM [True, True, True] `shouldBe` True
+
+  describe "With FOLD" $ do
+
+    it "product of a integer list" $
+      let actual = SUT.productFold [1, 2, 3, 4, 5]
+      in actual `shouldBe` 120
+
+    it "client with minimum name length" $
+      let actual = SUT.minimumClientFold [ Individual (Person "Fran" "Gonzalez" Male) True,
+                                     Individual (Person "Alicia" "Gonzalez" Female) True,
+                                     Individual (Person "Luis" "Gil" Unknown) True,
+                                     Company "ACME" 44864646 (Person "Fran" "Gonzalez" Male) "Boss",
+                                     GovOrg "Generalitat catalana" ]
+      in actual `shouldBe` Company "ACME" 44864646 (Person "Fran" "Gonzalez" Male) "Boss"
+
+    it "conjuntion of all conditions" $ do
+      SUT.allFold [True] `shouldBe` True
+      SUT.allFold [False] `shouldBe` False
+      SUT.allFold [True, False, True] `shouldBe` False
+      SUT.allFold [True, True, True] `shouldBe` True
+
+    it "minimum by" $ do
+      SUT.minimumBy (\x -> -x) [1, 2, 3] `shouldBe` 3
