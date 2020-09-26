133ae85f0253409fe2da9d0cf568b4546c56a7e1
Francisco González Ibáñez <fragonib@gmail.com>
2020-09-26 15:56:53 +0200

    Literal support

--- src/Kata/Knights.hs
+++ src/Kata/Knights.hs
@@ -1,12 +1,34 @@
 module Kata.Knights where
 
 import Data.List
+import Debug.Trace (trace)
 
 
 boardSize = 5
 
 type Knight = (Int, Int)
 type Board = [Knight]
+
+or' :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+or' f g a = f a || g a
+
+enumerate :: (Num a, Enum a) => [b] -> [(a, b)]
+enumerate = zip [0..]
+
+isValidSymbol :: Char -> Bool
+isValidSymbol = (== '.') `or'` (== 'k')
+
+isValidBoardLiteral :: [Char] -> Bool
+isValidBoardLiteral board | length board /= (boardSize * boardSize) = False
+isValidBoardLiteral board = foldr ((&&) . isValidSymbol) True board
+
+symbolsToBoard :: [Char] -> [Knight]
+symbolsToBoard = foldr symbolToKnight [] . enumerate
+
+symbolToKnight :: (Int, Char) -> [Knight] -> [Knight]
+symbolToKnight (index, symbol) knights = case symbol of
+  'k' -> index `divMod` boardSize : knights
+  _ -> knights
 
 isNineKnightBoard :: Board -> Bool
 isNineKnightBoard knights
--- test/Kata/KnightsSpec.hs
+++ test/Kata/KnightsSpec.hs
@@ -9,40 +9,82 @@ import qualified Kata.Knights as SUT
 spec :: Spec
 spec = do
 
-  describe "knight movements" $ do
+  describe "Symbols to board" $ do
 
-    it "when top-left position" $ do
-      SUT.knightMovements (0, 0) `shouldBe` [(1,2), (2,1)]
+    it "sample 1" $ do
+      SUT.symbolsToBoard "\
+          \...k.\
+          \...k.\
+          \k.k..\
+          \.k.k.\
+          \k.k.k" 
+        `shouldBe` [(0,3), (1,3), (2,0), (2,2), (3,1), (3,3), (4,0), (4,2), (4,4)]
+                                  
+    it "sample 2" $ do
+      SUT.symbolsToBoard "\
+          \.....\
+          \...k.\
+          \k.k.k\
+          \.k.k.\
+          \k.k.k" 
+        `shouldBe` [(1,3), (2,0), (2,2), (2,4), (3,1), (3,3), (4,0), (4,2), (4,4)]
+                                            
+    it "sample 2" $ do
+      SUT.symbolsToBoard "\
+          \.....\
+          \...k.\
+          \k.k.k\
+          \.k.k.\
+          \k...k" 
+        `shouldBe` [(1,3), (2,0), (2,2), (2,4), (3,1), (3,3), (4,0), (4,4)]
+
+
+  describe "Calculate knight movements" $ do
+
+    it "from top-left position" $ do
+      SUT.knightMovements (0, 0) 
+      `shouldBe` [(1,2), (2,1)]
       
-    it "when down-rigth position" $ do
-      SUT.knightMovements (4, 4) `shouldBe` [(2,3), (3,2)]
+    it "from down-rigth position" $ do
+      SUT.knightMovements (4, 4) 
+      `shouldBe` [(2,3), (3,2)]
       
-    it "when middle position" $ do
-      SUT.knightMovements (2, 2) `shouldBe` [(0,1), (0,3), (1,0), (1,4), (3,0), (3,4), (4,1), (4,3)]
+    it "from middle board position" $ do
+      SUT.knightMovements (2, 2) 
+      `shouldBe` [(0,1), (0,3), (1,0), (1,4), (3,0), (3,4), (4,1), (4,3)]
 
 
-  describe "valid knight board" $ do
+  describe "Is nine knight board" $ do
   
-    -- ...k.
-    -- ...k.
-    -- k.k..
-    -- .k.k.
-    -- k.k.k
-    it "board sample 1" $
-      SUT.isNineKnightBoard [(0,3), (1,3), (2,0), (2,2), (3,1), (3,3), (4,0), (4,2), (4,4)] `shouldBe` False
+    {-
+      ...k.
+      ...k.
+      k.k..
+      .k.k.
+      k.k.k
+    -}
+    it "sample 1" $
+      SUT.isNineKnightBoard [(0,3), (1,3), (2,0), (2,2), (3,1), (3,3), (4,0), (4,2), (4,4)] 
+      `shouldBe` False
 
-    -- .....
-    -- ...k.
-    -- k.k.k
-    -- .k.k.
-    -- k.k.k
-    it "board sample 2" $
-      SUT.isNineKnightBoard [(1,3), (2,0), (2,2), (2,4), (3,1), (3,3), (4,0), (4,2), (4,4)] `shouldBe` True
+    {-
+      .....
+      ...k.
+      k.k.k
+      .k.k.
+      k.k.k
+    -}
+    it "sample 2" $
+      SUT.isNineKnightBoard [(1,3), (2,0), (2,2), (2,4), (3,1), (3,3), (4,0), (4,2), (4,4)] 
+      `shouldBe` True
 
-    -- .....
-    -- ...k.
-    -- k.k.k
-    -- .k.k.
-    -- k...k
-    it "board sample 3" $
-      SUT.isNineKnightBoard [(1,3), (2,0), (2,2), (2,4), (3,1), (3,3), (4,0), (4,4)] `shouldBe` False
+    {-
+      .....
+      ...k.
+      k.k.k
+      .k.k.
+      k...k
+    -}
+    it "sample 3" $
+      SUT.isNineKnightBoard [(1,3), (2,0), (2,2), (2,4), (3,1), (3,3), (4,0), (4,4)] 
+      `shouldBe` False
