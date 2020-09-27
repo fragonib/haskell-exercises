976f88f3a3220bd9d50e435fd58c620a80b65938
Francisco González Ibáñez <fragonib@gmail.com>
2020-09-27 18:30:45 +0200

    Initial version

---
+++ src/Kata/MusicalNotation.hs
@@ -0,0 +1,23 @@
+module Kata.MusicalNotation where
+
+import Data.Char
+
+data Note =
+  SimpleNote { symbol :: Char } |
+  RepeatedNote { symbol :: Char , repetition :: Int }
+  deriving (Show, Eq)
+
+musicalNotationCLI :: [Char] -> [Note]
+musicalNotationCLI l = foldr symbolToNote [] $ words l
+
+symbolToNote :: [Char] -> [Note] -> [Note]
+symbolToNote (symbol:repetition) notes
+ | null repetition = SimpleNote symbol : notes
+ | otherwise = RepeatedNote symbol (digitToInt $ head repetition) : notes
+
+
+main :: IO()
+main = do
+    l1 <- getLine
+    l2 <- getLine
+    mapM_ print $ musicalNotationCLI $ trace "ads" l2
