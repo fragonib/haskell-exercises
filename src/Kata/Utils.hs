5f9bfb6d4ea1f9c73a5e704d837416ecaeb336ef
Francisco González Ibáñez <fragonib@gmail.com>
2020-10-08 15:20:39 +0200

    REFACTOR: Move revert to utils

--- src/Kata/Utils.hs
+++ src/Kata/Utils.hs
@@ -30,3 +30,9 @@ leftTruncate _ [] = []
 leftTruncate _ [single] = [single]
 leftTruncate el l@(first:rest) =
   if first == el then leftTruncate el rest else l
+  
+reverse :: [a] -> [a]
+reverse l = rev l []
+  where
+    rev []     a = a
+    rev (x:xs) a = rev xs (x:a)
