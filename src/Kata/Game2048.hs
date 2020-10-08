c78d0681d555acb383355c2d8893832c20063e99
Francisco González Ibáñez <fragonib@gmail.com>
2020-10-08 18:25:14 +0200

    Resolve left movement

--- src/Kata/Game2048.hs
+++ src/Kata/Game2048.hs
@@ -1,34 +1,41 @@
 module Kata.Game2048 where
 
-import qualified Kata.Utils as U
 import Data.List
 import Data.Char (digitToInt)
+import Debug.Trace (trace)
+import qualified Kata.Utils as U
 
 
 type Row = [Int]
 type Board = [Row]
-data Movement = 
-  Left | 
-  Right | 
-  Up | 
-  Down
+data Movement =
+  Left | Right | Up | Down
   deriving (Show)
 
 
 -- Movement
 
-game2048 :: Board -> Movement -> Board
-game2048 board mov = map (`moveRow` mov) board
+moveBoard :: Board -> Movement -> Board
+moveBoard board mov = map (`moveRow` mov) board
 
 moveRow :: Row -> Movement -> Row
-moveRow row m = 
-  U.rightPadZero 4 added
-    where shift = foldr (\x acum -> if x /= 0 then x:acum else acum) [] row
-          added = foldr aggregateRow [] shift
-        
-aggregateRow :: Int -> [Int] -> [Int]      
-aggregateRow e [] = [e]
-aggregateRow e r@(y:ys) = if e == y then e*2:ys else e:r
+moveRow row m = U.rightPadZero 4 $ aggregateRow $ filter (/=0) row
+
+-- foldl (flip $ U.trace2 aggregateRow) [] row
+
+aggregateRowFold :: Int -> [Int] -> [Int]
+aggregateRowFold 0 rowRest = rowRest
+aggregateRowFold el [] = [el]
+aggregateRowFold el rowRest@(x:xs) = if el == x then xs ++ [2*el]
+                                            else rowRest ++ [el]
+
+aggregateRow :: [Int] -> [Int]
+aggregateRow [] = []
+aggregateRow [x] = [x]
+aggregateRow (x:y:others) = if x == y then x+y : aggregateRow others
+                                      else x   : aggregateRow (y : others)
+
+
 
 
 -- IO
@@ -37,7 +44,7 @@ parseBoard :: [String] -> Board
 parseBoard = map parseRow
 
 parseRow :: String -> Row
-parseRow ls = map (\x -> read x :: Int) (words ls)
+parseRow ls = map (\num -> read num :: Int) (words ls)
 
 parseMovement :: String -> Movement
 parseMovement movLine = case digitToInt (head movLine) of
@@ -57,17 +64,17 @@ printRow row = unwords $ map show row
 -- CLI
 
 game2048CLI :: String -> String -> String -> String -> String -> String
-game2048CLI row1 row2 row3 row4 movLine =
-  printBoard (let board = parseBoard [row1, row2, row3, row4]
-                  movement = parseMovement movLine
-              in game2048 board movement)
+game2048CLI row1Line row2Line row3Line row4Line movLine =
+  printBoard $ moveBoard board movement
+  where board = parseBoard [row1Line, row2Line, row3Line, row4Line]
+        movement = parseMovement movLine
 
 main :: IO()
 main = do
-    row1 <- getLine
-    row2 <- getLine
-    row3 <- getLine
-    row4 <- getLine
-    movSymbol <- getLine
-    putStrLn $ game2048CLI row1 row2 row3 row4 movSymbol
+  row1Line <- getLine
+  row2Line <- getLine
+  row3Line <- getLine
+  row4Line <- getLine
+  movementLine <- getLine
+  putStrLn $ game2048CLI row1Line row2Line row3Line row4Line movementLine
 
