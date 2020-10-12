42a04a4432a45c1d8d530844228b580cd39ea7d0
Francisco González Ibáñez <fragonib@gmail.com>
2020-10-12 12:05:18 +0200

    Word Cloud algorithm

--- src/Kata/WordCloud.hs
+++ src/Kata/WordCloud.hs
@@ -1,33 +1,90 @@
 module Kata.WordCloud where
 
+import Data.List (sortOn)
+
+
+type WordHistogram = [WordFreq]
+type WordFreq = (String, Int)
+type WordSize = (Int, Int) -- FontSize, Width
+
+
 -- Word Cloud
 
-calculateCloudHeight :: Int -> Int -> [(String, Int)] -> Int
-calculateCloudHeight maxWidth maxWords histogram = 5
+calculateCloudHeight :: Int -> Int -> WordHistogram -> Int
+calculateCloudHeight maxLineWidth maxWords wordHistogram =
+  sum $
+  cloudLineHeights maxLineWidth $
+  wordsInCloud maxWords wordHistogram
 
+wordsInCloud :: Int -> WordHistogram -> WordHistogram
+wordsInCloud maxWords wordHistogram =
+  take maxWords $
+  sortOn fst $
+  filter ((<5) . snd) wordHistogram
+
+cloudLineHeights :: Int -> WordHistogram -> [Int]
+cloudLineHeights maxLineWidth wordHistogram =
+  lineHeight : cloudLineHeights maxLineWidth (drop wordsCount wordHistogram)
+  where (lineHeight, wordsCount) = cloudLineHeight maxLineWidth wordHistogram
+
+cloudLineHeight :: Int -> WordHistogram -> (Int, Int)
+cloudLineHeight maxLineWidth wordHistogram = (maxWordHeight, wordsCountThatFitInLine)
+  where maxWordHeight = maximum $ take wordsCountThatFitInLine $ map fst wordSizesList
+        wordsCountThatFitInLine = wordsThatFitInLine maxLineWidth wordSizesList
+        wordSizesList = wordSizes wordHistogram
+
+wordSizes :: WordHistogram -> [WordSize]
+wordSizes wordHistogram =
+  map (wordSizeHeight maxFreq) wordHistogram
+    where maxFreq = maxWordFreq wordHistogram
+
+wordsThatFitInLine :: Int -> [WordSize] -> Int
+wordsThatFitInLine maxLineWidth wordSizes =
+  length $
+  takeWhile (< maxLineWidth) $
+  scanl (+) 0 $
+  map snd wordSizes
+
+wordSizeHeight :: Int -> WordFreq -> (Int, Int)
+wordSizeHeight maxFreq (word, wordFreq) =
+  let fontSize = wordHeightInPts maxFreq wordFreq
+      width = wordWidthInPts word fontSize
+   in (fontSize, width)
+
+maxWordFreq :: WordHistogram -> Int
+maxWordFreq ls = maximum $ map snd ls
+
+wordHeightInPts :: Int -> Int -> Int
+wordHeightInPts maxOccurrences actualOccurrences =
+  8 + (40 * (actualOccurrences - 4)) `ceilDiv` (maxOccurrences - 4)
+
+wordWidthInPts :: [Char] -> Int -> Int
+wordWidthInPts word fontSize = 
+  (9 * length word * fontSize) `ceilDiv` 16
+
+ceilDiv :: Int -> Int -> Int
+a `ceilDiv` b = (a + b - 1) `div` b
 
 -- IO
 
-cloudSize :: String -> [Int]
-cloudSize firstLine = map read $ words firstLine
+extractCloudSize :: String -> [Int]
+extractCloudSize firstLine = map read $ words firstLine
 
-wordHistogram :: [String] -> [(String, Int)]
-wordHistogram = map wordOccurrences 
+extractWordHistogram :: [String] -> WordHistogram
+extractWordHistogram = map extractWordOccurrences
 
-wordOccurrences :: String -> (String, Int)
-wordOccurrences ls = 
-  case words ls of
-    (word:frequency:_) -> (word, read frequency)
+extractWordOccurrences :: String -> WordFreq
+extractWordOccurrences ls = case words ls of (word:frequency:_) -> (word, read frequency)
 
 outputHeight :: Int -> Int -> String
-outputHeight index height = "CLOUD " ++ show index ++ ": " ++ show height 
+outputHeight index height = "CLOUD " ++ show index ++ ": " ++ show height
 
 wordCloudCLI :: [String] -> String
 wordCloudCLI inputLines =
-  let maxWidth = head (cloudSize (head inputLines))
-      maxWords = head $ tail $ cloudSize (head inputLines)
-      histogram = wordHistogram $ takeWhile (/= "0 0") $ tail inputLines
-      cloudHeight = calculateCloudHeight maxWidth maxWords histogram
+  let maxWidth = head (extractCloudSize (head inputLines))
+      maxWords = head $ tail $ extractCloudSize (head inputLines)
+      wordHistogram = extractWordHistogram $ takeWhile (/= "0 0") $ tail inputLines
+      cloudHeight = calculateCloudHeight maxWidth maxWords wordHistogram
    in outputHeight 1 cloudHeight
 
 main :: IO()
--- test/Kata/WordCloudSpec.hs
+++ test/Kata/WordCloudSpec.hs
@@ -9,6 +9,11 @@ import qualified Kata.WordCloud as SUT
 spec :: Spec
 spec = do
 
+  describe "word sizes" $ do
+
+    it "word width in points" $ do
+      SUT.wordWidthInPts "words" 20 `shouldBe` 57
+
   describe "WordCloud" $ do
 
     it "sample 1" $ do
@@ -20,8 +25,8 @@ spec = do
         ("kiwi", 18),
         ("orange", 12),
         ("strawberry", 10)]
-      `shouldBe` 114    
-      
+      `shouldBe` 114
+
     it "sample 2" $ do
 
       SUT.calculateCloudHeight 250 6 [
@@ -31,8 +36,8 @@ spec = do
         ("kiwi", 18),
         ("orange", 12),
         ("strawberry", 10)]
-      `shouldBe` 99    
-      
+      `shouldBe` 99
+
     it "sample 3" $ do
 
       SUT.calculateCloudHeight 610 6 [
