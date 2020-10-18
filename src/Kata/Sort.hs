de6612e8a7c2e7d426996e0d32d84e20eb8d5a17
Francisco González Ibáñez <fragonib@gmail.com>
2020-10-18 23:10:25 +0200

    [SORT] First passing version

--- src/Kata/Sort.hs
+++ src/Kata/Sort.hs
@@ -1,30 +1,42 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-
 module Kata.Sort where
 
-import Data.List (intercalate, groupBy, group, sortBy)
-import qualified Data.Map as M
 import Data.Maybe (fromJust)
+import Data.List (sortBy)
+import qualified Data.Map as M
 
 
-type FreqMap = M.Map Int Int
+type IntMap = M.Map Int Int
 
 
 -- Sort
 
 sort :: [Int] -> [Int]
-sort xs = sortBy (frequencyComp $ histogram xs) xs
+sort xs = sortBy (sortComparison fm pm) xs
+  where fm = frequencyMap xs
+        pm = firstPositionMap xs
 
-histogram :: [Int] -> FreqMap
-histogram xs = M.fromListWith (+) $ zip xs (repeat 1)
+sortComparison :: IntMap -> IntMap -> Int -> Int -> Ordering
+sortComparison frequencyMap firstPositionMap a b =
+  case frequencyComp frequencyMap a b of
+    EQ -> positionComp firstPositionMap a b
+    h -> h
 
---histogram2 :: [Int] -> [(Int, Int)]
---histogram2 xs = map (\g -> (head g, length g)) $ groupBy (==) xs
+frequencyComp :: IntMap -> Int -> Int -> Ordering
+frequencyComp frequencies x y = compare freq2 freq1
+  where freq1 = fromJust $ M.lookup x frequencies
+        freq2 = fromJust $ M.lookup y frequencies
 
-frequencyComp :: FreqMap -> Int -> Int -> Ordering
-frequencyComp histo x y = compare freq2 freq1
-  where freq1 = fromJust $ M.lookup x histo
-        freq2 = fromJust $ M.lookup y histo
+frequencyMap :: [Int] -> IntMap
+frequencyMap xs = M.fromListWith (+) $ zip xs (repeat 1)
+
+positionComp :: IntMap -> Int -> Int -> Ordering
+positionComp positions x y = compare pos1 pos2
+  where pos1 = fromJust $ M.lookup x positions
+        pos2 = fromJust $ M.lookup y positions
+
+firstPositionMap :: [Int] -> IntMap
+firstPositionMap xs = M.fromListWith (curry snd) $ zip xs [1..]
 
 
 -- IO
--- test/Kata/SortSpec.hs
+++ test/Kata/SortSpec.hs
@@ -3,16 +3,27 @@ module Kata.SortSpec where
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
+import qualified Data.Map as M
 import qualified Kata.Sort as SUT
 
 
 spec :: Spec
 spec = do
 
+  describe "Position map" $ do
+
+    it "sample1" $ do
+      SUT.firstPositionMap [2,1,2,1,2] `shouldBe` M.fromList [(2,1),(1,2)]
+
+  describe "Frequency map" $ do
+
+    it "sample1" $ do
+      SUT.frequencyMap [2,1,2,1,2] `shouldBe` M.fromList [(2,3),(1,2)]
+
   describe "Sort" $ do
 
     it "sample1" $ do
-      SUT.sort [2, 1, 2, 1, 2] `shouldBe` [2, 2, 2, 1, 1]
+      SUT.sort [2,1,2,1,2] `shouldBe` [2,2,2,1,1]
 
     it "sample2" $ do
       SUT.sort [1,3,3,3,2,2,2,1,1] `shouldBe` [1,1,1,3,3,3,2,2,2]
