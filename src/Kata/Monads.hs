9638c17e457dfb0ff5c11b633589535fff2b063f
Francisco González Ibáñez <fragonib@gmail.com>
2020-12-07 19:33:13 +0100

    Monads

--- src/Kata/Monads.hs
+++ src/Kata/Monads.hs
@@ -1,32 +1,63 @@
 module Kata.Monads where
 
+import Control.Monad.Writer
+import Data.Monoid (mappend)
+
+
 -- Dada la función
 
 add :: Monad m => m Int -> (Int -> m Int) -> m Int
-add prodA prodB = do
-    a <- prodA
-    b <- prodB a
+add valueM func = do
+    a <- valueM
+    b <- func a
     return $ a + b
+
+-- Do Notation
+
+-- add :: Maybe String
+-- add = Just 3 >>= (\x ->
+--       Just 5 >>= (\y ->
+--       Just (a + b)))
+
+-- To save us from writing all these annoying lambdas, Haskell gives us do notation.
+-- It allows us to write the previous piece of code like this:
+
+-- foo :: Maybe String
+-- foo = do
+--     x <- Just 3
+--     y <- Just 5
+--     Just (x + y)
 
 -- Escribir las funciones siguientes de modo que hagan lo mismo que add,
 -- pero sin usar ni la notación 'do' ni los operadores '>>' y '>>='
 
+-- Add Maybe
+
 addMaybe :: Maybe Int -> (Int -> Maybe Int) -> Maybe Int
 addMaybe Nothing _ = Nothing
-addMaybe m@(Just x) f = (+) <$> m <*> f x
+addMaybe m@(Just a) f = (+) <$> m <*> f a
+
+-- Add List
 
 addList :: [Int] -> (Int -> [Int]) -> [Int]
 addList [] _ = []
-addList l f = concatMap (\x -> map (x+) (f x)) l 
+addList l f = [a + b | a <- l, b <- f a]
 
--- addWriter :: Writer Int -> (Int -> Writer Int) -> Writer Int
+-- Add Writer
+
+type IntWriterM = Writer String Int
+
+intWriter :: Int -> String -> IntWriterM
+intWriter value logLine = writer (value, logLine)
+
+addWriter :: IntWriterM -> (Int -> IntWriterM) -> IntWriterM
+addWriter initialWriter operation =
+  let (value, monoid) = runWriter initialWriter
+      (value', monoid') = runWriter $ operation value
+  in intWriter (value + value') (monoid `mappend` monoid')
 
 -- addReader :: (Int -> Int) -> (Int -> (Int -> Int)) -> (Int -> Int)
 
--- addState :: State Int Int -> (Int -> State Int Int) -> State Int Int
-
-
--- Asumir las declaraciones siguientes:
-
-newtype Writer w a = Writer (a, w) deriving Show
 newtype State s a = State { runState :: s -> (a, s) }
+
+-- addState :: State Int Int -> (Int -> State Int Int) -> State Int Int
--- test/Kata/MonadsSpec.hs
+++ test/Kata/MonadsSpec.hs
@@ -3,6 +3,7 @@ module Kata.MonadsSpec where
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
+import Control.Monad.Writer 
 import qualified Kata.Monads as SUT
 
 
@@ -12,7 +13,15 @@ spec = do
   describe "add monad" $ do
 
     it "addMaybe" $ do
-       SUT.addMaybe (Just 2) (\x -> Just (x*3)) `shouldBe` SUT.add (Just 2) (\x -> Just (x*3))
+       SUT.addMaybe (Just 2) (\x -> Just (x*3)) 
+          `shouldBe` SUT.add (Just 2) (\x -> Just (x*3))
 
     it "addList" $ do
-       SUT.addList [1, 2, 3] (\x -> [x-1, x+1]) `shouldBe` SUT.add [1, 2, 3] (\x -> [x-1, x+1])
+       SUT.addList [1, 2, 3] (\x -> [x-1, x+1]) 
+          `shouldBe` SUT.add [1, 2, 3] (\x -> [x-1, x+1])
+           
+    it "addWriter" $ do
+       SUT.addWriter (SUT.intWriter 2 "Input: 2") (\x -> SUT.intWriter (x*3) ("Oper: " ++ show (x*3)))
+          `shouldBe` SUT.add (SUT.intWriter 2 "Input: 2") (\x -> SUT.intWriter (x*3) ("Oper: " ++ show (x*3)))
+          
+        
